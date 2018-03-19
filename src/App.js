03bd3d4ab1b170c371cd251a050b24091bfa36c5
Andy Hang <andy@llt-group.com>
2018-03-19 15:52:04 -0500

    resume json, and updated about.js to render each json item

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import About from './components/about';
 import Header from './components/header';
 import Footer from './components/footer';
+import { resume } from './components/resume.json';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(resume)
     return (
       <div className="App">
         <Header />
-        <About />
+        <About 
+          about={resume.about} 
+          certificates={resume.certificates} 
+          experiences={resume.experiences} />
         <Footer />
       </div>
     );
--- src/components/about.js
+++ src/components/about.js
@@ -3,35 +3,14 @@ import './about.css';
 
 class About extends Component {
   render() {
-    const certificates = [
-      {
-        title: 'Full Stack Web Developer Certificate',
-        company: 'Northwestern Coding Bootcamp',
-        length: '2017'
-      },
-      {
-        title: 'Front-End Web Developer Nanodegree',
-        company: 'UDACITY',
-        length: '2016'
-      }
-    ];
-    const experiences = [
-      {
-        title: 'Web Application Developer',
-        company: 'LLT Group',
-        length: '2017 - Present'
-      },
-      {
-        title: 'QA & Forms Specialist',
-        company: 'Best Case, LLC',
-        length: '2016 - 2017',
-      },
-      {
-        title: 'Tech Support Specialist',
-        company: 'Best Case, LLC',
-        length: '2013 - 2016'
-      }
-    ];
+    const certificates = this.props.certificates;
+    const experiences = this.props.experiences;
+    const about = this.props.about;
+
+    const aboutMap = about.map(about => {
+      return <p className="block__about">{ about }</p>
+    })
+
     const certMap = certificates.map(certifcate => {
       return <div className="block">
         <h5>{ certifcate.title }</h5>
@@ -57,8 +36,7 @@ class About extends Component {
             <i className="far fa-user"></i>
             <h3>About Me</h3>
             <div className="block">
-              <p class="block__about">I discovered my passion for coding through conversations I had with my previous company on how our business' software was built. The logic and functionality of each piece of the system was very intriguing, and I began researching opportutnies to further my edification in software development fundamentals. I decided to try UDACITY, and after the completion of the Front-End Web Developer course I realized that I wanted more! I went through and received my certification as a Full Stack Developer through <a href="https://bootcamp.northwestern.edu/coding/">Northwestern University's Coding Bootcamp</a>.</p>
-              <p className="block__about">I currently am working full time at LLT Group as a Web App Developer. I am hoping to build my experience as a developer and to give back to the open-source community</p>
+              { aboutMap }
             </div>
           </div>
           <div className="col">
