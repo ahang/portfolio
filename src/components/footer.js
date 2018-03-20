98e33e59e5165bd665a70ca867cf3cac26e3c0c3
Andy Hang <andy@llt-group.com>
2018-03-20 17:18:39 -0500

    updated footer slightly, updated reusme, created skill component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return(
-    <div className="footer">
+    <footer className="footer">
       <p>Down Here</p>
-    </div>
+    </footer>
   )
 }
 
---
+++ src/components/skill.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Skill extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Skill;
